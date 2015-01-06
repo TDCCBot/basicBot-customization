4810e5778030530652466db58e6b31b7320ca157
TDCCBot <okto123@rocketmail.com>
2015-01-06 16:23:32 +0800

    Update extensionpatch.js

--- extensionpatch.js
+++ extensionpatch.js
@@ -46,17 +46,18 @@
         };
 
         bot.commands.helloCommand = {
-            command: 'hello'
-            rank: 'user'
-            type: 'exact'
+            command: 'hello',
+            rank: 'user',
+            type: 'exact',
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API. sendChat("/me Hello there. Welcome to IndieGoogle! Enjoy!")
+                    API. sendChat("/me Hello there. Welcome to IndieGoogle! Enjoy!");
                 }
             }
-        }
+        };
+        
         //Load the chat package again to account for any changes
         bot.loadChat();
 
