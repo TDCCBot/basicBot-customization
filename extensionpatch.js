93891d8fd036873dd5dad723252d9d7f1d5d1773
TDCCBot <okto123@rocketmail.com>
2015-01-09 23:29:51 +0800

    Update extensionpatch.js

--- extensionpatch.js
+++ extensionpatch.js
@@ -71,6 +71,19 @@
             }
         };
         
+        bot.commands.catfactsCommand = {
+            command: 'catfacts',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank,chat)) return void (0);
+                else {
+                    API.sendchat("/me catfacts are still under construction!")
+                }
+            }
+        };
+        
         //Load the chat package again to account for any changes
         bot.loadChat();
 
